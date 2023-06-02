1e9a6b01970e6926363f8c2a6b53a4693f8afd08
hong tran <hong@elsanow.io>
2023-06-02 10:57:05 +0700

    fix route gp

--- src/index.js
+++ src/index.js
@@ -4,15 +4,12 @@ import './index.css';
 import App from './App';
 import './App.css';
 import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/videosharingUI">
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
 
