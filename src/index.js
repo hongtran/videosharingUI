5249f3396a267a40455c9f197120b0fa563cd880
hong tran <hong@elsanow.io>
2023-06-02 09:14:12 +0700

    fix route for gp

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <HashRouter>
+    <BrowserRouter basename="/videosharingUI">
       <App />
-    </HashRouter>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
